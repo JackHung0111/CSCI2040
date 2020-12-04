e77c12d6e7edbc4fbadb14015db961d5445e540f
JackHung <62652318+JackHung0111@users.noreply.github.com>
2020-12-04 23:51:34 +0800

    Update p1.py

--- lab2/p1.py
+++ lab2/p1.py
@@ -2,16 +2,8 @@
 # lab 2 p1
 
 def check_sublist(list1, d1, d2):
-    lista = []
-    listb = []
-    listc = []
-    for integer in list1:
-        if (integer > d1 * d2):
-            lista.append(integer)
-        if (integer < d1 * d2):
-            listb.append(integer)
-        if (integer < d1 or integer < d2):
-            listc.append(integer)
-    return lista, listb, listc
+    lista = [integer for integer in list1 if integer > d1*d2]
+    listb = [integer for integer in list1 if integer < d1*d2]
+    listc = [integer for integer in list1 if integer < d1 or integer < d2]
 
-# print(check_sublist([21, 25, 4, 6, 28], 3, 7))
+    return lista, listb, listc
